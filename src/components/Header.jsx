fa224f175283c3b07f1b9ede6154ca7a505866a9
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-09-04 00:55:34 +0400

    Modify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,33 @@ import { Link } from 'react-router-dom';
 
 import { useCartItemContext } from '../contexts/ShopContext.jsx';
 
-import logo from '../assets/logo.png';
-import cart from '../assets/shopping-cart.png';
+import cart from '../assets/cart.svg';
 
 const Header = () => {
   const { totalQuantity } = useCartItemContext();
 
   return (
     <header className="header common-pd">
-      <div className="logo">
-        <img src={logo} alt="shopping bag" className="logo-img" />
-        <p className="logo-text">STORE LOGO</p>
+      <div className="header-logo-wrapper">
+        <p className="header-logo-text">
+          <span className="header-logo-text-large">A</span>DD
+          <span className="header-logo-text-large">T</span>O
+          <span className="header-logo-text-large">C</span>ART
+        </p>
       </div>
-      <div className="header-links">
+      <div className="header-links-wrapper">
         <Link to="/" className="header-link">
           HOME
         </Link>
         <Link to="shop" className="header-link">
           SHOP
         </Link>
-        <Link to="cart" className="cart-link">
-          <img src={cart} alt="cart" className="cart-icon" />
-          <div className="cart-quantity-wrapper">
-            <p className="cart-quanitity">{totalQuantity}</p>
+      </div>
+      <div className="header-cart-link-wrapper">
+        <Link to="cart" className="header-cart-link">
+          <img src={cart} alt="cart" className="header-cart-icon" />
+          <div className="header-cart-quantity-wrapper">
+            <p className="header-cart-quanitity">{totalQuantity}</p>
           </div>
         </Link>
       </div>
