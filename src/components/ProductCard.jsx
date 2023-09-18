2fe686e7a75887701b4877c66fde1336e62f09e0
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-09-18 12:21:51 +0400

    Replace context provider and modify design

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 
-import { useProductContext, useCartItemContext } from '../contexts/ShopContext.jsx';
+import useProductStore from '../stores/productStore.js';
+import { useCartItemContext } from '../contexts/ShopContext.jsx';
 
 const ProductCard = ({ productId }) => {
-  const { products } = useProductContext();
+  const { products } = useProductStore();
   const { isInCart } = useCartItemContext();
   const product = products.find((product) => product.id === productId);
 
@@ -11,32 +12,23 @@ const ProductCard = ({ productId }) => {
     return null;
   }
 
-  const {
-    id,
-    node: {
-      title,
-      featuredImage: { url },
-      variants,
-    },
-  } = product;
+  const { id, name, price, imageUrl } = product;
 
-  const { amount } = variants.edges[0].node.price;
-
-  const price = parseFloat(amount).toFixed(2);
+  const formattedPrice = parseFloat(price).toFixed(2);
 
   return (
-    <div className="product-card | flex flex-col gap-2 text-xs relative | md:text-sm">
+    <div id="product-card" className="flex flex-col gap-2 text-xs relative md:text-sm">
       {isInCart(id) ? (
-        <div className="flex items-center justify-center bg-amber-300 text-black text-2xs absolute top-0 w-full | md:text-xs">
+        <div className="flex items-center justify-center bg-amber-300 text-black text-2xs absolute top-0 w-full md:text-xs">
           ITEM IN CART
         </div>
       ) : null}
       <Link to={`/shop/product/${id}`}>
-        <img src={url} alt={title} className="product-card-image | h-full w-full object-cover" />
+        <img src={imageUrl} alt={name} className="h-full w-full object-cover" />
       </Link>
       <div className="flex flex-col gap-0.5">
-        <p>{title}</p>
-        <p className="font-bold">AED {price}</p>
+        <p>{name}</p>
+        <p className="font-bold">AED {formattedPrice}</p>
       </div>
     </div>
   );
