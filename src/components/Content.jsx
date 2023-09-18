5fa8887e51e78193f2a6edaa7d09c5cda9be759e
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-09-18 12:01:10 +0400

    Remove style

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Content = () => {
   return (
-    <main className="content | flex justify-center">
+    <main id="content">
       <Outlet />
     </main>
   );
